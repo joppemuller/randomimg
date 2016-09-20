3a4993d200cf398273dac846119b7786c370f07a
joppemuller <joppe@thekids.eu>
2016-09-21 01:08:05 +0200

    V1

---
+++ js/imgload.js
@@ -0,0 +1,17 @@
+var arrayImg = new Array();
+arrayImg[0] = "image1.jpeg";
+arrayImg[1] = "image2.jpeg";
+arrayImg[2] = "image3.jpeg";
+arrayImg[3] = "image4.jpeg";
+arrayImg[4] = "image5.jpeg";
+arrayImg[5] = "image6.jpeg";
+
+getRandomImage(arrayImg, "");
+
+function getRandomImage(imgAr, path) {
+    path = path || 'images/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<img src="' + path + img + '" alt = "">';
+    document.write(imgStr); document.close();
+}
